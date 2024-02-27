480a012b83bbcef1806b4425051bd44e0e488c30
zent2g <155169236+zent2g@users.noreply.github.com>
2024-02-27 19:09:39 +0100

    Add loop for grid and cells

---
+++ script.js
@@ -0,0 +1,16 @@
+const divContainer = document.querySelector('.gridContainer');
+const grid = document.createElement('div-grid');
+
+function createGrids(){
+    for(let i = 0; i < 16; i++){
+        for(let j = 0; j < 16; j++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            grid.appendChild(cell)
+        }
+    }
+}
+
+createGrids();
+
+divContainer.appendChild(grid);
